e379eab52a90a1957e1737ccbc9882649dbc74a1
lzzy12 <jhashivam2020@gmail.com>
2018-06-15 06:46:46 +0000

    Use if..elif structure
    Signed-off-by: lzzy12 <jhashivam2020@gmail.com>

--- tg_bot/modules/sudo_promote_demote.py
+++ tg_bot/modules/sudo_promote_demote.py
@@ -1,4 +1,4 @@
-import html, json
+import html
 from typing import Optional, List
 from telegram import Message, Chat, Update, Bot, User
 from telegram import ParseMode
@@ -8,7 +8,7 @@ from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import escape_markdown, mention_html
 from tg_bot.modules.helper_funcs.filters import CustomFilters
 from tg_bot.modules.helper_funcs.extraction import extract_user
-from tg_bot import dispatcher, SUDO_USERS, OWNER_USERNAME
+from tg_bot import dispatcher, SUDO_USERS, OWNER_USERNAME, OWNER_ID
 
 with open("sudo_users.txt", 'r') as file:
     for line in file:
@@ -17,7 +17,7 @@ with open("sudo_users.txt", 'r') as file:
 
 def add_to_sudo(user_id):
     with open("sudo_users.txt", 'a') as outfile:
-        outfile.write(user_id)
+        outfile.write(str(user_id) + "\n")
     SUDO_USERS.append(user_id)   #So that bot need not to be restarted after each gpromotions
 
 
@@ -31,17 +31,20 @@ def gpromote(bot: Bot, update: Update):
         if not user_id:
             message.reply_text("You don't seems to be referring to a user.")
             return
-        if int(user_id) in SUDO_USERS:
+        elif int(user_id) in SUDO_USERS:
             message.reply_text("The user is already a sudo user.")
             return
-        if int(user_id) == OWNER_ID:
+        elif int(user_id) == OWNER_ID:
             message.reply_text("The specified user is my owner! No need add him to SUDO_USERS list!")
             return
-        add_to_sudo(user_id)
+        else:
+            add_to_sudo(user_id)
+            message.reply_text("{} is now a SUDO user!!".format(user_id.username))
+            return
     else:
         message.reply_text("Only owner of the bot, {} can do this".format(OWNER_USERNAME))
         return
 
-GPROMOTE_HANDLER = CommandHandler("gpromote", gpromote, pass_args=True, filters = CustomFilters.sudo_filter)
+GPROMOTE_HANDLER = CommandHandler("gpromote", gpromote, filters = CustomFilters.sudo_filter)
 dispatcher.add_handler(GPROMOTE_HANDLER)
 
