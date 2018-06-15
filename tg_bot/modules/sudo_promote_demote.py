69192865718181699da7b0b7d7bf3d52e0df07e0
lzzy12 <jhashivam2020@gmail.com>
2018-06-15 06:23:35 +0000

    Only allow owner to add sudo user
    Signed-off-by: lzzy12 <jhashivam2020@gmail.com>

--- tg_bot/modules/sudo_promote_demote.py
+++ tg_bot/modules/sudo_promote_demote.py
@@ -1,4 +1,4 @@
-import html
+import html, json
 from typing import Optional, List
 from telegram import Message, Chat, Update, Bot, User
 from telegram import ParseMode
@@ -8,7 +8,7 @@ from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import escape_markdown, mention_html
 from tg_bot.modules.helper_funcs.filters import CustomFilters
 from tg_bot.modules.helper_funcs.extraction import extract_user
-from tg_bot import dispatcher, SUDO_USERS, OWNER_ID
+from tg_bot import dispatcher, SUDO_USERS, OWNER_USERNAME
 
 with open("sudo_users.txt", 'r') as file:
     for line in file:
@@ -17,28 +17,31 @@ with open("sudo_users.txt", 'r') as file:
 
 def add_to_sudo(user_id):
     with open("sudo_users.txt", 'a') as outfile:
-        outfile.write(str(user_id))
+        outfile.write(user_id)
     SUDO_USERS.append(user_id)   #So that bot need not to be restarted after each gpromotions
+
 
 @run_async
 def gpromote(bot: Bot, update: Update):
     args = List[str]
     message = update.effective_message
+    banner = update.effective_user
     user_id = extract_user(message, args)
-    print("user_id declared")
-    if not user_id:
-        message.reply_text("You don't seems to be referring to a user.")
-        return
-    if int(user_id) in SUDO_USERS:
-        message.reply_text("The user is already a sudo user.")
-        return
-    if int(user_id) == OWNER_ID:
-        message.reply_text("The specified user is my owner! No need add him to SUDO_USERS list!")
-        return
-    else:
+    if banner.id == OWNER_ID:
+        if not user_id:
+            message.reply_text("You don't seems to be referring to a user.")
+            return
+        if int(user_id) in SUDO_USERS:
+            message.reply_text("The user is already a sudo user.")
+            return
+        if int(user_id) == OWNER_ID:
+            message.reply_text("The specified user is my owner! No need add him to SUDO_USERS list!")
+            return
         add_to_sudo(user_id)
-        message.reply_text("Succesfully added to sudo user list")
+    else:
+        message.reply_text("Only owner of the bot, {} can do this".format(OWNER_USERNAME))
+        return
 
-GPROMOTE_HANDLER = CommandHandler("gpromote", gpromote,  filters = CustomFilters.sudo_filter)
+GPROMOTE_HANDLER = CommandHandler("gpromote", gpromote, pass_args=True, filters = CustomFilters.sudo_filter)
 dispatcher.add_handler(GPROMOTE_HANDLER)
 
