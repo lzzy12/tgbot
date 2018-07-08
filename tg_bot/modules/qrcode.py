3565d2930eb9fa5b99b1045f1cb96f796e7d9f41
lzzy12 <jhashivam2020@gmail.com>
2018-07-08 10:33:24 +0000

    qrcode: Delete the file from disk once our purpose is done
    Signed-off-by: lzzy12 <jhashivam2020@gmail.com>

--- tg_bot/modules/qrcode.py
+++ tg_bot/modules/qrcode.py
@@ -1,14 +1,16 @@
+import os
 import pyqrcode
 from typing import List
 from tg_bot import dispatcher
 from telegram import Message, Chat, Update, Bot, ParseMode
 from telegram.ext import CommandHandler, MessageHandler, Filters, run_async
 
+qr_file = "qr-file.png"
 def gen_qrcode(args: List[str]):
     char = str()
     for i in args:
         char += i + " "
-    pyqrcode.create(char).png("new.png", scale=4)
+    pyqrcode.create(char).png(qr_file, scale=4)
 
 @run_async
 def send_qrcode(bot: Bot, update: Update, args: List[str]):
@@ -16,7 +18,8 @@ def send_qrcode(bot: Bot, update: Update, args: List[str]):
     chat = update.effective_chat
     message.reply_text("Processing!")
     gen_qrcode(args)
-    bot.send_photo(chat.id, open("new.png", 'rb'), caption="Done!")
+    bot.send_photo(chat.id, open(qr_file, 'rb'), caption="Done!")
+    os.remove(qr_file)
 
 QR_HANDLER = CommandHandler("qr", send_qrcode, pass_args=True)
 dispatcher.add_handler(QR_HANDLER)
